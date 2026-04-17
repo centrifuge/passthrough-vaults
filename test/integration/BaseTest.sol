1efabc921654c1d86a967f5de7eac8e38e95d1d2
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2026-04-17 10:17:40 +0200

    inherit integration test

---
+++ test/integration/BaseTest.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import {CentrifugeIntegrationTestWithUtils} from "protocol-test/integration/Integration.t.sol";
+import {IntegrationConstants} from "protocol-test/integration/utils/IntegrationConstants.sol";
+
+import {D18, d18} from "protocol/misc/types/D18.sol";
+import {MAX_MESSAGE_COST as GAS} from "protocol/core/messaging/interfaces/IGasService.sol";
+import {CastLib} from "protocol/misc/libraries/CastLib.sol";
+import {AccountId} from "protocol/core/types/AccountId.sol";
+import {VaultUpdateKind} from "protocol/core/messaging/libraries/MessageLib.sol";
+import {IHubRequestManager} from "protocol/core/hub/interfaces/IHubRequestManager.sol";
+import {ISyncManager} from "protocol/vaults/interfaces/IVaultManagers.sol";
+import {UpdateRestrictionMessageLib} from "protocol/hooks/libraries/UpdateRestrictionMessageLib.sol";
+import {SyncDepositVault} from "protocol/vaults/SyncDepositVault.sol";
+import {BatchRequestManager} from "protocol/vaults/BatchRequestManager.sol";
+
+import {PassthroughVault} from "../../src/PassthroughVault.sol";
+
+/// @notice Base contract for PassthroughVault integration tests.
+///         Extends CentrifugeIntegrationTestWithUtils and provides helpers to deploy a
+///         SyncDepositVault and a PassthroughVault using real Hub↔Spoke messaging.
+contract IntegrationBaseTest is CentrifugeIntegrationTestWithUtils {
+    using CastLib for *;
+    using UpdateRestrictionMessageLib for *;
+
+    uint128 constant HOOK_GAS = IntegrationConstants.HOOK_GAS;
+    uint128 constant EXTRA_GAS = IntegrationConstants.EXTRA_GAS;
+
+    AccountId constant ASSET_ACCOUNT = IntegrationConstants.ASSET_ACCOUNT;
+    AccountId constant EQUITY_ACCOUNT = IntegrationConstants.EQUITY_ACCOUNT;
+
+    function setUp() public virtual override {
+        super.setUp();
+        vm.deal(FM, 10 ether);
+    }
+
+    function _deploySyncDepositVault() internal returns (SyncDepositVault vault, uint128 assetId) {
+        _createPool();
+
+        vm.startPrank(FM);
+
+        hub.createAccount(POOL_A, ASSET_ACCOUNT, true);
+        hub.createAccount(POOL_A, EQUITY_ACCOUNT, false);
+
+        hub.notifyPool{value: GAS}(POOL_A, LOCAL_CENTRIFUGE_ID, FM);
+        hub.notifyShareClass{value: GAS}(
+            POOL_A, SC_1, LOCAL_CENTRIFUGE_ID, address(freelyTransferableHook).toBytes32(), FM
+        );
+        vm.stopPrank();
+
+        _registerUSDC();
+        assetId = usdcId.raw();
+
+        vm.startPrank(FM);
+
+        hub.initializeHolding(
+            POOL_A, SC_1, usdcId, identityValuation, ASSET_ACCOUNT, EQUITY_ACCOUNT, EQUITY_ACCOUNT, EQUITY_ACCOUNT
+        );
+        hub.setRequestManager{value: GAS}(
+            POOL_A,
+            LOCAL_CENTRIFUGE_ID,
+            IHubRequestManager(address(batchRequestManager)),
+            address(asyncRequestManager).toBytes32(),
+            FM
+        );
+        hub.updateBalanceSheetManager{value: GAS}(
+            POOL_A, LOCAL_CENTRIFUGE_ID, address(asyncRequestManager).toBytes32(), true, FM
+        );
+        hub.updateBalanceSheetManager{value: GAS}(
+            POOL_A, LOCAL_CENTRIFUGE_ID, address(syncManager).toBytes32(), true, FM
+        );
+
+        hub.updateSharePrice(POOL_A, SC_1, d18(1e18), uint64(block.timestamp));
+        hub.notifySharePrice{value: GAS}(POOL_A, SC_1, LOCAL_CENTRIFUGE_ID, FM);
+        hub.notifyAssetPrice{value: GAS}(POOL_A, SC_1, usdcId, FM);
+
+        hub.updateVault{value: GAS}(
+            POOL_A,
+            SC_1,
+            usdcId,
+            address(syncDepositVaultFactory).toBytes32(),
+            VaultUpdateKind.DeployAndLink,
+            EXTRA_GAS,
+            FM
+        );
+
+        hub.updateContract{value: GAS}(
+            POOL_A,
+            SC_1,
+            LOCAL_CENTRIFUGE_ID,
+            address(syncManager).toBytes32(),
+            _updateContractSyncDepositMaxReserveMsg(usdcId, type(uint128).max),
+            0,
+            FM
+        );
+        vm.stopPrank();
+
+        vault = SyncDepositVault(address(vaultRegistry.vault(POOL_A, SC_1, usdcId, asyncRequestManager)));
+    }
+
+    function _deployPassthroughVault(SyncDepositVault underlying, bool allowPermissionlessClaiming)
+        internal
+        returns (PassthroughVault pv)
+    {
+        pv = new PassthroughVault(address(underlying), address(0), allowPermissionlessClaiming);
+
+        vm.prank(FM);
+        hub.updateRestriction{value: GAS}(
+            POOL_A,
+            SC_1,
+            LOCAL_CENTRIFUGE_ID,
+            UpdateRestrictionMessageLib.UpdateRestrictionMember({
+                    user: address(pv).toBytes32(), validUntil: type(uint64).max
+                }).serialize(),
+            EXTRA_GAS,
+            FM
+        );
+    }
+
+    function _settleRedeem(PassthroughVault pv, uint128 shares) internal {
+        vm.startPrank(FM);
+
+        uint32 redeemEpochId = batchRequestManager.nowRedeemEpoch(POOL_A, SC_1, usdcId);
+        D18 pricePoolPerAsset_ = hub.pricePoolPerAsset(POOL_A, SC_1, usdcId);
+        batchRequestManager.approveRedeems(POOL_A, SC_1, usdcId, redeemEpochId, shares, pricePoolPerAsset_);
+
+        uint32 revokeEpochId = batchRequestManager.nowRevokeEpoch(POOL_A, SC_1, usdcId);
+        (D18 sharePrice,) = shareClassManager.pricePoolPerShare(POOL_A, SC_1);
+        batchRequestManager.revokeShares{value: GAS}(POOL_A, SC_1, usdcId, revokeEpochId, sharePrice, HOOK_GAS, FM);
+
+        bytes32 investor = address(pv).toBytes32();
+        batchRequestManager.notifyRedeem{value: GAS}(
+            POOL_A, SC_1, usdcId, investor, batchRequestManager.maxRedeemClaims(POOL_A, SC_1, investor, usdcId), FM
+        );
+
+        vm.stopPrank();
+    }
+}
--- test/integration/PassthroughVault.t.sol
+++ test/integration/PassthroughVault.t.sol
@@ -2,87 +2,69 @@
 pragma solidity 0.8.28;
 
 import {ERC20} from "protocol/misc/ERC20.sol";
-
-import {BaseTest, VaultKind} from "protocol-test/core/spoke/integration/BaseTest.sol";
-
 import {SyncDepositVault} from "protocol/vaults/SyncDepositVault.sol";
-import {PassthroughVault} from "../../src/PassthroughVault.sol";
 
-contract PassthroughVaultTest is BaseTest {
-    uint128 constant INITIAL_ASSET_BALANCE = 5_000e6;
+import {PassthroughVault} from "../../src/PassthroughVault.sol";
+import {IntegrationBaseTest} from "./BaseTest.sol";
+
+contract PassthroughVaultTest is IntegrationBaseTest {
+    uint128 constant INITIAL_BALANCE = 5_000e6;
+
+    // Share token has 18 decimals, USDC has 6 — scale factor between asset and share amounts.
+    uint256 constant SHARE_SCALE = 1e12;
 
     address immutable INVESTOR = makeAddr("INVESTOR");
     address immutable INVESTOR2 = makeAddr("INVESTOR2");
 
-    uint64 poolId;
-    uint128 assetId;
-    bytes16 scId;
     SyncDepositVault underlying;
     PassthroughVault passthroughVault;
+    uint128 assetId;
 
     function setUp() public override {
         super.setUp();
-        address vaultAddress;
-        (poolId, vaultAddress, assetId) = deployVault(
-            VaultKind.SyncDepositAsyncRedeem,
-            6,
-            address(freelyTransferableHook),
-            defaultShareClassId,
-            address(erc20),
-            erc20TokenId,
-            OTHER_CHAIN_ID
-        );
-        underlying = SyncDepositVault(vaultAddress);
-        scId = underlying.scId().raw();
-        passthroughVault = new PassthroughVault(address(underlying), address(0), false);
-        centrifugeChain.updateMember(poolId, scId, address(passthroughVault), type(uint64).max);
 
-        erc20.mint(INVESTOR, INITIAL_ASSET_BALANCE);
-        erc20.mint(INVESTOR2, INITIAL_ASSET_BALANCE);
+        (underlying, assetId) = _deploySyncDepositVault();
+        passthroughVault = _deployPassthroughVault(underlying, false);
+
+        _mintUSDC(INVESTOR, INITIAL_BALANCE);
+        _mintUSDC(INVESTOR2, INITIAL_BALANCE);
 
         vm.startPrank(INVESTOR);
-        erc20.approve(address(passthroughVault), type(uint256).max);
+        usdc.approve(address(passthroughVault), type(uint256).max);
         ERC20(underlying.share()).approve(address(passthroughVault), type(uint256).max);
         vm.stopPrank();
 
         vm.startPrank(INVESTOR2);
-        erc20.approve(address(passthroughVault), type(uint256).max);
+        usdc.approve(address(passthroughVault), type(uint256).max);
         ERC20(underlying.share()).approve(address(passthroughVault), type(uint256).max);
         vm.stopPrank();
     }
 
     function testFullFlow() public {
         uint256 depositAmount = 1000e6;
+        uint256 expectedShares = depositAmount * SHARE_SCALE;
 
         vm.prank(INVESTOR);
         uint256 sharesMinted = passthroughVault.deposit(depositAmount, INVESTOR);
 
-        assertEq(sharesMinted, depositAmount);
-        assertEq(ERC20(underlying.share()).balanceOf(INVESTOR), sharesMinted);
+        assertEq(sharesMinted, expectedShares);
+        assertEq(ERC20(underlying.share()).balanceOf(INVESTOR), expectedShares);
 
         vm.prank(INVESTOR);
-        passthroughVault.requestRedeem(sharesMinted, INVESTOR, INVESTOR);
+        passthroughVault.requestRedeem(expectedShares, INVESTOR, INVESTOR);
 
         assertEq(ERC20(underlying.share()).balanceOf(INVESTOR), 0);
-        assertEq(passthroughVault.pendingRedeemRequest(0, INVESTOR), sharesMinted);
+        assertEq(passthroughVault.pendingRedeemRequest(0, INVESTOR), expectedShares);
 
-        centrifugeChain.isFulfilledRedeemRequest(
-            poolId,
-            scId,
-            bytes32(bytes20(address(passthroughVault))),
-            assetId,
-            uint128(sharesMinted),
-            uint128(sharesMinted),
-            0
-        );
-        assertEq(underlying.maxWithdraw(address(passthroughVault)), sharesMinted);
+        _settleRedeem(passthroughVault, uint128(expectedShares));
+        assertEq(underlying.maxWithdraw(address(passthroughVault)), depositAmount);
 
-        uint256 balanceBefore = erc20.balanceOf(INVESTOR);
+        uint256 balanceBefore = usdc.balanceOf(INVESTOR);
         vm.prank(INVESTOR);
-        uint256 assetsReceived = passthroughVault.redeem(sharesMinted, INVESTOR, INVESTOR);
+        uint256 assetsReceived = passthroughVault.redeem(expectedShares, INVESTOR, INVESTOR);
 
-        assertEq(assetsReceived, sharesMinted);
-        assertEq(erc20.balanceOf(INVESTOR) - balanceBefore, sharesMinted);
+        assertEq(assetsReceived, depositAmount);
+        assertEq(usdc.balanceOf(INVESTOR) - balanceBefore, depositAmount);
         assertEq(underlying.maxWithdraw(address(passthroughVault)), 0);
     }
 
@@ -90,24 +72,24 @@ contract PassthroughVaultTest is BaseTest {
     // The orphaned and overlapping segments resolve when the settlement catches up, only affecting
     // the user who re-requested. In the end everybody should get the correct amount of assets.
     //
-    // Queue after first two requests:
-    //           0                    1000                  2000
+    // Queue after first two requests (in share units):
+    //           0                    1000e18               2000e18
     //           |--------------------|---------------------|
     //           [      User A        ][      User B        ]
     //
-    // Queue after first partial settlement:
-    //           0          500        1000                 2000
-    //           |XXXXXXXXXX|----------|--------------------|
-    //           [ Settled  ][ User A  ][      User B       ]
+    // Queue after first partial settlement (500e18 settled):
+    //           0        500e18       1000e18              2000e18
+    //           |XXXXXXXX|------------|---------------------|
+    //           [Settled ][ User A   ][      User B        ]
     //
-    // Queue after User A re-requests:
-    //           0          500                1000              1500             2000            2500
-    //           |XXXXXXXXXX|------------------|-----------------|----------------|---------------|
-    //           [ Settled  ][ Orphaned (500) ][              User B              ]
-    //                                                           [ Overlap (500)  ]
-    //                                                           [          User A (1000)         ]
+    // Queue after User A re-requests (500e18 new shares):
+    //           0        500e18              1000e18            1500e18           2000e18         2500e18
+    //           |XXXXXXXX|-------------------|------------------|-----------------|----------------|
+    //           [Settled ][ Orphaned (500e18)][         User B                   ]
+    //                                                           [Overlap (500e18) ]
+    //                                                           [        User A (1000e18)          ]
     function testRequestAgainWithPartialFulfillment() public {
-        // INVESTOR deposits 1500e6 so they have 500e6 shares left after the first requestRedeem (1000e6),
+        // INVESTOR deposits 1500e6 so they have 500e18 shares left after the first requestRedeem (1000e18),
         // which they use for the second requestRedeem that triggers the force-claim and re-queuing.
         vm.prank(INVESTOR);
         passthroughVault.deposit(1500e6, INVESTOR);
@@ -115,61 +97,51 @@ contract PassthroughVaultTest is BaseTest {
         passthroughVault.deposit(1000e6, INVESTOR2);
 
         vm.prank(INVESTOR);
-        passthroughVault.requestRedeem(1000e6, INVESTOR, INVESTOR);
+        passthroughVault.requestRedeem(1000e18, INVESTOR, INVESTOR);
         vm.prank(INVESTOR2);
-        passthroughVault.requestRedeem(1000e6, INVESTOR2, INVESTOR2);
+        passthroughVault.requestRedeem(1000e18, INVESTOR2, INVESTOR2);
 
-        centrifugeChain.isFulfilledRedeemRequest(
-            poolId, scId, bytes32(bytes20(address(passthroughVault))), assetId, 500e6, 500e6, 0
-        );
+        _settleRedeem(passthroughVault, 500e18);
 
-        assertEq(passthroughVault.maxRedeem(INVESTOR), 500e6);
+        assertEq(passthroughVault.maxRedeem(INVESTOR), 500e18);
         assertEq(passthroughVault.maxRedeem(INVESTOR2), 0);
 
         vm.prank(INVESTOR);
-        passthroughVault.requestRedeem(500e6, INVESTOR, INVESTOR);
+        passthroughVault.requestRedeem(500e18, INVESTOR, INVESTOR);
 
         // Investor A's position should be re-queued to [oldCumulative - oldPending, oldCumulative + newlyRequested]
-        // = [1500, 2500]
+        // = [1500e18, 2500e18]
         (uint128 rangeStart, uint128 pending) = passthroughVault.redeemPosition(INVESTOR);
-        assertEq(rangeStart, 1500e6);
-        assertEq(pending, 1000e6);
+        assertEq(rangeStart, 1500e18);
+        assertEq(pending, 1000e18);
 
-        assertEq(passthroughVault.pendingRedeemRequest(0, INVESTOR), 1000e6);
-        assertEq(passthroughVault.cumulativeRedeemRequested(), 2500e6);
+        assertEq(passthroughVault.pendingRedeemRequest(0, INVESTOR), 1000e18);
+        assertEq(passthroughVault.cumulativeRedeemRequested(), 2500e18);
         assertEq(passthroughVault.maxRedeem(INVESTOR), 0);
         assertEq(passthroughVault.maxRedeem(INVESTOR2), 0);
 
-        centrifugeChain.isFulfilledRedeemRequest(
-            poolId, scId, bytes32(bytes20(address(passthroughVault))), assetId, 500e6, 500e6, 0
-        );
+        _settleRedeem(passthroughVault, 500e18);
 
         // Settling the orphaned segment won't make anything claimable
         assertEq(passthroughVault.maxRedeem(INVESTOR), 0);
         assertEq(passthroughVault.maxRedeem(INVESTOR2), 0);
 
-        centrifugeChain.isFulfilledRedeemRequest(
-            poolId, scId, bytes32(bytes20(address(passthroughVault))), assetId, 500e6, 500e6, 0
-        );
+        _settleRedeem(passthroughVault, 500e18);
 
-        // Settling [1000, 1500] makes 500 shares claimable for investor B only
+        // Settling [1000e18, 1500e18] makes 500e18 shares claimable for investor B only
         assertEq(passthroughVault.maxRedeem(INVESTOR), 0);
-        assertEq(passthroughVault.maxRedeem(INVESTOR2), 500e6);
+        assertEq(passthroughVault.maxRedeem(INVESTOR2), 500e18);
 
-        centrifugeChain.isFulfilledRedeemRequest(
-            poolId, scId, bytes32(bytes20(address(passthroughVault))), assetId, 500e6, 500e6, 0
-        );
+        _settleRedeem(passthroughVault, 500e18);
 
-        // Settling the overlapping segment makes the remaining 500 claimable for B and 1000 for A
-        assertEq(passthroughVault.maxRedeem(INVESTOR), 500e6);
-        assertEq(passthroughVault.maxRedeem(INVESTOR2), 1000e6);
+        // Settling the overlapping segment makes the remaining 500e18 claimable for B and 1000e18 for A
+        assertEq(passthroughVault.maxRedeem(INVESTOR), 500e18);
+        assertEq(passthroughVault.maxRedeem(INVESTOR2), 1000e18);
 
-        centrifugeChain.isFulfilledRedeemRequest(
-            poolId, scId, bytes32(bytes20(address(passthroughVault))), assetId, 500e6, 500e6, 0
-        );
+        _settleRedeem(passthroughVault, 500e18);
 
         // Final settlement: everything fully claimable for both investors
-        assertEq(passthroughVault.maxRedeem(INVESTOR), 1000e6);
-        assertEq(passthroughVault.maxRedeem(INVESTOR2), 1000e6);
+        assertEq(passthroughVault.maxRedeem(INVESTOR), 1000e18);
+        assertEq(passthroughVault.maxRedeem(INVESTOR2), 1000e18);
     }
 }
